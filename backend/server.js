68c3665e67ae867518dbe8561e0dc436ef8dab5e
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-08 23:43:31 +0300

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import dotenv from "dotenv";
+import connectDB from './db/connectDB.js';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-app.listen(5000, () => console.log('Server started at http://localhost:5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
