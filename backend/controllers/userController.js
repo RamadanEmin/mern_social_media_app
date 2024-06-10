98a7ed707d7b7b7b6e50972b61dab689dbc4f385
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-10 18:32:04 +0300

    Added logout controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,7 +67,18 @@ const loginUser = async (req, res) => {
 	}
 };
 
+const logoutUser = (req, res) => {
+	try {
+		res.cookie('jwt', '', { maxAge: 1 });
+		res.status(200).json({ message: 'User logged out successfully!' });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+		console.log('Error in signupUser: ', err.message);
+	}
+};
+
 export {
 	signupUser,
-	loginUser
+	loginUser,
+	logoutUser
 };
