dae452caee8ac7fc2d0d5bf0533df8e9f55668d5
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-12 14:44:37 +0300

    Added Reply post controller

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -79,7 +79,7 @@ const likeUnlikePost = async (req, res) => {
 
         const userLikedPost = post.likes.includes(userId);
 
-        if (userLikedPost) {      
+        if (userLikedPost) {
             await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
             res.status(200).json({ message: 'Post unliked successfully.' });
         } else {
@@ -92,4 +92,38 @@ const likeUnlikePost = async (req, res) => {
     }
 };
 
-export { createPost, getPost, deletePost, likeUnlikePost };
+const replyToPost = async (req, res) => {
+    try {
+        const { text } = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+        const userProfilePic = req.user.profilePic;
+        const username = req.user.username;
+
+        if (!text) {
+            return res.status(400).json({ error: 'Text field is required!' });
+        }
+
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.status(404).json({ error: 'Post not found!' });
+        }
+
+        const reply = { userId, text, userProfilePic, username };
+
+        post.replies.push(reply);
+        await post.save();
+
+        res.status(200).json(reply);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+export {
+    createPost,
+    getPost,
+    deletePost,
+    likeUnlikePost,
+    replyToPost
+};
