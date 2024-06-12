2288bc8ffb146e2a71b83e954bbfcc16bdbbbefc
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-12 15:46:38 +0300

    Added Get feed controller

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import { createPost, deletePost, getPost, likeUnlikePost, replyToPost } from '../controllers/postController.js';
+import {
+    createPost,
+    deletePost,
+    getFeedPosts,
+    getPost,
+    likeUnlikePost,
+    replyToPost
+} from '../controllers/postController.js';
 import protectRoute from '../middlewares/protectRoute.js';
 
 const router = express.Router();
 
+router.get('/feed', protectRoute, getFeedPosts);
 router.post('/create', protectRoute, createPost);
 router.get('/:id', getPost);
 router.delete('/:id', protectRoute, deletePost);
