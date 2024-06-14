4c12b19b88af621747dbc6b7c08acfe7e280d404
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-14 14:37:32 +0300

    Added Submit login

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -17,10 +17,47 @@ import { useState } from 'react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import { useSetRecoilState } from 'recoil';
 import authScreenAtom from '../atoms/authAtom';
+import userAtom from '../atoms/userAtom';
+import useShowToast from '../hooks/useShowToast';
 
 export default function LoginCard() {
     const [showPassword, setShowPassword] = useState(false);
     const setAuthScreen = useSetRecoilState(authScreenAtom);
+    const setUser = useSetRecoilState(userAtom);
+
+    const [loading, setLoading] = useState(false);
+    const [inputs, setInputs] = useState({
+        username: '',
+        password: '',
+    });
+
+    const showToast = useShowToast();
+
+    const handleLogin = async () => {
+        setLoading(true);
+        try {
+            const res = await fetch('/api/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(inputs),
+            });
+
+            const data = await res.json();
+            if (data.error) {
+                showToast('Error', data.error, 'error');
+                return;
+            }
+            
+            localStorage.setItem('user-threads', JSON.stringify(data));
+            setUser(data);
+        } catch (error) {
+            showToast('Error', error, 'error');
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <Flex align={'center'} justify={'center'}>
@@ -43,12 +80,20 @@ export default function LoginCard() {
                     <Stack spacing={4}>
                         <FormControl isRequired>
                             <FormLabel>Username</FormLabel>
-                            <Input type="text" />
+                            <Input
+                                type="text"
+                                value={inputs.username}
+                                onChange={(e) => setInputs((inputs) => ({ ...inputs, username: e.target.value }))}
+                            />
                         </FormControl>
                         <FormControl isRequired>
                             <FormLabel>Password</FormLabel>
                             <InputGroup>
-                                <Input type={showPassword ? 'text' : 'password'} />
+                                <Input
+                                    type={showPassword ? 'text' : 'password'}
+                                    value={inputs.password}
+                                    onChange={(e) => setInputs((inputs) => ({ ...inputs, password: e.target.value }))}
+                                />
                                 <InputRightElement h={'full'}>
                                     <Button
                                         variant={'ghost'}
@@ -68,7 +113,10 @@ export default function LoginCard() {
                                 color={'white'}
                                 _hover={{
                                     bg: useColorModeValue('gray.700', 'gray.800')
-                                }}>
+                                }}
+                                onClick={handleLogin}
+                                isLoading={loading}
+                            >
                                 Login
                             </Button>
                         </Stack>
