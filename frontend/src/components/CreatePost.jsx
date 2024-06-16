0701055ef56bbcc299c762df27cecba84b69deb3
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-16 17:29:51 +0300

    Added Create post component

---
+++ frontend/src/components/CreatePost.jsx
@@ -0,0 +1,94 @@
+import { AddIcon } from '@chakra-ui/icons';
+import {
+    Button,
+    CloseButton,
+    Flex,
+    FormControl,
+    Image,
+    Input,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Text,
+    Textarea,
+    useColorModeValue,
+    useDisclosure,
+} from '@chakra-ui/react';
+import { useRef } from 'react';
+import usePreviewImg from '../hooks/usePreviewImg';
+import { BsFillImageFill } from 'react-icons/bs';
+
+const CreatePost = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { handleImageChange, imgUrl, setImgUrl } = usePreviewImg();
+    const imageRef = useRef(null);
+
+    return (
+        <>
+            <Button
+                position={'fixed'}
+                bottom={10}
+                right={5}
+                bg={useColorModeValue('gray.300', 'gray.dark')}
+                onClick={onOpen}
+                size={{ base: 'sm', sm: 'md' }}
+            >
+                <AddIcon />
+            </Button>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+
+                <ModalContent>
+                    <ModalHeader>Create Post</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <FormControl>
+                            <Textarea
+                                placeholder='Post content goes here..'
+                            />
+                            <Text fontSize='xs' fontWeight='bold' textAlign={'right'} m={'1'} color={'gray.800'}>
+                                500/500
+                            </Text>
+
+                            <Input type='file' hidden ref={imageRef} onChange={handleImageChange} />
+
+                            <BsFillImageFill
+                                style={{ marginLeft: '5px', cursor: 'pointer' }}
+                                size={16}
+                                onClick={() => imageRef.current.click()}
+                            />
+                        </FormControl>
+
+                        {imgUrl && (
+                            <Flex mt={5} w={'full'} position={'relative'}>
+                                <Image src={imgUrl} alt='Selected img' />
+                                <CloseButton
+                                    onClick={() => {
+                                        setImgUrl('');
+                                    }}
+                                    bg={'gray.800'}
+                                    position={'absolute'}
+                                    top={2}
+                                    right={2}
+                                />
+                            </Flex>
+                        )}
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} >
+                            Post
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
+};
+
+export default CreatePost;
