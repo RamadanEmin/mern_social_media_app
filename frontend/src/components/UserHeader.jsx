dd7a3569dad53d1d1209da25b1bf0945c31f3c37
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-16 13:04:49 +0300

    Update User header component with user props

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -2,12 +2,18 @@ import { Avatar } from '@chakra-ui/avatar';
 import { Box, Flex, Link, Text, VStack } from '@chakra-ui/layout';
 import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/menu';
 import { Portal } from '@chakra-ui/portal';
-import { useToast } from '@chakra-ui/react';
+import { Button, useToast } from '@chakra-ui/react';
 import { BsInstagram } from 'react-icons/bs';
 import { CgMoreO } from 'react-icons/cg';
+import { useRecoilValue } from 'recoil';
+import userAtom from '../atoms/userAtom';
+import { Link as RouterLink } from 'react-router-dom';
+import { useState } from 'react';
 
-const UserHeader = () => {
+const UserHeader = ({ user }) => {
     const toast = useToast();
+    const currentUser = useRecoilValue(userAtom);
+    const [following, setFollowing] = useState(user.followers.includes(currentUser._id));
 
     const copyURL = () => {
         const currentURL = window.location.href;
@@ -27,32 +33,54 @@ const UserHeader = () => {
             <Flex justifyContent={'space-between'} w={'full'}>
                 <Box>
                     <Text fontSize={'2xl'} fontWeight={'bold'}>
-                        Mark Zuckerberg
+                        {user.name}
                     </Text>
                     <Flex gap={2} alignItems={'center'}>
-                        <Text fontSize={'sm'}>markzuckerberg</Text>
+                        <Text fontSize={'sm'}>{user.username}</Text>
                         <Text fontSize={'xs'} bg={'gray.dark'} color={'gray.light'} p={1} borderRadius={'full'}>
                             threads.net
                         </Text>
                     </Flex>
                 </Box>
                 <Box>
-                    <Avatar
-                        name="Mark Zuckerberg"
-                        src="/zuck-avatar.png"
-                        size={{
-                            base: 'md',
-                            md: 'xl'
-                        }}
-                    />
+                    {user.profilePic && (
+                        <Avatar
+                            name={user.name}
+                            src={user.profilePic}
+                            size={{
+                                base: 'md',
+                                md: 'xl'
+                            }}
+                        />
+                    )}
+                    {!user.profilePic && (
+                        <Avatar
+                            name={user.name}
+                            src='https://bit.ly/broken-link'
+                            size={{
+                                base: 'md',
+                                md: 'xl'
+                            }}
+                        />
+                    )}
                 </Box>
             </Flex>
 
-            <Text>Co-founder, axecutive chairman and CEO of Meta Platforms.</Text>
+            <Text>{user.bio}</Text>
+            {currentUser?._id === user._id && (
+                <Link as={RouterLink} to='/update'>
+                    <Button size={'sm'}>Update Profile</Button>
+                </Link>
+            )}
+            {currentUser?._id !== user._id && (
+                <Button size={'sm'} >
+                    {following ? 'Unfollow' : 'Follow'}
+                </Button>
+            )}
 
             <Flex w={'full'} justifyContent={'space-between'}>
                 <Flex gap={2} alignItems={'center'}>
-                    <Text color={'gray.light'}>3.2K followers</Text>
+                    <Text color={'gray.light'}>{user.followers.length} followers</Text>
                     <Box w='1' h='1' bg={'gray.light'} borderRadius={'full'}></Box>
                     <Link color={'gray.light'}>instagram.com</Link>
                 </Flex>
