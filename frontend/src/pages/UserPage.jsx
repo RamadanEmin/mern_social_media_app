b31aa10a51a0e5c0fb2c60f13d32a3b08e74d603
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-17 13:48:50 +0300

    Update Header component

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -3,10 +3,12 @@ import UserHeader from '../components/UserHeader';
 import UserPost from '../components/UserPost';
 import { useParams } from 'react-router-dom';
 import useShowToast from '../hooks/useShowToast';
+import { Flex, Spinner } from '@chakra-ui/react';
 
 const UserPage = () => {
     const [user, setUser] = useState(null);
     const { username } = useParams();
+    const [loading, setLoading] = useState(true);
 
     const showToast = useShowToast();
 
@@ -24,14 +26,24 @@ const UserPage = () => {
                 setUser(data);
             } catch (error) {
                 showToast('Error', error, 'error');
+            } finally {
+                setLoading(false);
             }
         };
 
         getUser();
     }, [username, showToast]);
 
-    if (!user) {
-        return null;
+    if (!user && loading) {
+        return (
+            <Flex justifyContent={'center'}>
+                <Spinner size={'xl'} />
+            </Flex>
+        );
+    }
+
+    if (!user && !loading) {
+        return <h1>User not found</h1>;
     }
 
     return (
