2f118b8ead597fe9665b2cec0693aaafbc9753cf
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-18 00:40:56 +0300

    Update post replies

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,17 +1,19 @@
 import { Avatar } from '@chakra-ui/avatar';
 import { Image } from '@chakra-ui/image';
 import { Box, Flex, Text } from '@chakra-ui/layout';
-import { BsThreeDots } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Actions from './Actions';
 import { useEffect, useState } from 'react';
 import useShowToast from '../hooks/useShowToast';
+import { formatDistanceToNow } from 'date-fns';
 
 const Post = ({ post, postedBy }) => {
     const [liked, setLiked] = useState(false);
     const [user, setUser] = useState(null);
 
     const showToast = useShowToast();
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const getUser = async () => {
@@ -38,54 +40,76 @@ const Post = ({ post, postedBy }) => {
     }
 
     return (
-        <Link to={'/markzuckerberg/post/1'}>
+        <Link to={`/${user.username}/post/${post._id}`}>
             <Flex gap={3} mb={4} py={5}>
                 <Flex flexDirection={'column'} alignItems={'center'}>
-                    <Avatar size='md' name={user?.name} src={user?.profilePic} />
+                    <Avatar
+                        size='md'
+                        name={user?.name}
+                        src={user?.profilePic}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            navigate(`/${user.username}`);
+                        }}
+                    />
                     <Box w='1px' h={'full'} bg='gray.light' my={2}></Box>
                     <Box position={'relative'} w={'full'}>
-                        <Avatar
-                            size='xs'
-                            name='John doe'
-                            src='https://bit.ly/dan-abramov'
-                            position={'absolute'}
-                            top={'0px'}
-                            left='15px'
-                            padding={'2px'}
-                        />
-                        <Avatar
-                            size='xs'
-                            name='John doe'
-                            src='https://bit.ly/sage-adebayo'
-                            position={'absolute'}
-                            bottom={'0px'}
-                            right='-5px'
-                            padding={'2px'}
-                        />
-                        <Avatar
-                            size='xs'
-                            name='John doe'
-                            src='https://bit.ly/prosper-baba'
-                            position={'absolute'}
-                            bottom={'0px'}
-                            left='4px'
-                            padding={'2px'}
-                        />
+                        {post.replies.length === 0 && <Text textAlign={'center'}>🥱</Text>}
+                        {post.replies[0] && (
+                            <Avatar
+                                size='xs'
+                                name='John doe'
+                                src={post.replies[0].userProfilePic}
+                                position={'absolute'}
+                                top={'0px'}
+                                left='15px'
+                                padding={'2px'}
+                            />
+                        )}
+                        {post.replies[1] && (
+                            <Avatar
+                                size='xs'
+                                name='John doe'
+                                src={post.replies[1].userProfilePic}
+                                position={'absolute'}
+                                bottom={'0px'}
+                                right='-5px'
+                                padding={'2px'}
+                            />
+                        )}
+
+                        {post.replies[2] && (
+                            <Avatar
+                                size='xs'
+                                name='John doe'
+                                src={post.replies[2].userProfilePic}
+                                position={'absolute'}
+                                bottom={'0px'}
+                                left='4px'
+                                padding={'2px'}
+                            />
+                        )}
                     </Box>
                 </Flex>
                 <Flex flex={1} flexDirection={'column'} gap={2}>
                     <Flex justifyContent={'space-between'} w={'full'}>
                         <Flex w={'full'} alignItems={'center'}>
-                            <Text fontSize={'sm'} fontWeight={'bold'}>
+                            <Text
+                                fontSize={'sm'}
+                                fontWeight={'bold'}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    navigate(`/${user.username}`);
+                                }}
+                            >
                                 {user?.username}
                             </Text>
                             <Image src='/verified.png' w={4} h={4} ml={1} />
                         </Flex>
                         <Flex gap={4} alignItems={'center'}>
-                            <Text fontStyle={'sm'} color={'gray.light'}>
-                                1d
+                            <Text fontSize={'xs'} width={36} textAlign={'right'} color={'gray.light'}>
+                                {formatDistanceToNow(new Date(post.createdAt))} ago
                             </Text>
-                            <BsThreeDots />
                         </Flex>
                     </Flex>
 
