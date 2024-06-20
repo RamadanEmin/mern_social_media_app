52bb3a0eff7ff79ddc14381cd5fb6d42e00b025e
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-20 15:10:59 +0300

    Added Delete post functionality from post details page

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,23 +1,28 @@
 import { Avatar, Box, Button, Divider, Flex, Image, Spinner, Text } from '@chakra-ui/react';
 import Actions from '../components/Actions';
 import Comment from '../components/Comment';
-import { BsThreeDots } from 'react-icons/bs';
 import useGetUserProfile from '../hooks/useGetUserProfile';
 import useShowToast from '../hooks/useShowToast';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { formatDistanceToNow } from 'date-fns';
+import { useRecoilValue } from 'recoil';
+import userAtom from '../atoms/userAtom';
+import { DeleteIcon } from '@chakra-ui/icons';
 
 const PostPage = () => {
     const { user, loading } = useGetUserProfile();
     const [post, setPost] = useState(null);
 
+    const currentUser = useRecoilValue(userAtom);
+
     const { pid } = useParams();
+    const navigate = useNavigate();
 
     const showToast = useShowToast();
 
     useEffect(() => {
         const getPost = async () => {
-            // setPost([]);
             try {
                 const res = await fetch(`/api/posts/${pid}`);
                 const data = await res.json();
@@ -35,6 +40,29 @@ const PostPage = () => {
         };
         getPost();
     }, [showToast, pid]);
+
+    const handleDeletePost = async () => {
+		try {
+			if (!window.confirm('Are you sure you want to delete this post?')){
+                return;
+            }
+
+			const res = await fetch(`/api/posts/${post._id}`, {
+				method: 'DELETE',
+			});
+			const data = await res.json();
+
+			if (data.error) {
+				showToast('Error', data.error, 'error');
+				return;
+			}
+
+			showToast('Success', 'Post deleted', 'success');
+			navigate(`/${user.username}`);
+		} catch (error) {
+			showToast('Error', error.message, 'error');
+		}
+	};
 
     if (!user && loading) {
         return (
@@ -62,28 +90,25 @@ const PostPage = () => {
                 </Flex>
                 <Flex gap={4} alignItems={'center'}>
                     <Text fontSize={'xs'} width={36} textAlign={'right'} color={'gray.light'}>
-                        1d
+                        {formatDistanceToNow(new Date(post.createdAt))} ago
                     </Text>
-                    <BsThreeDots />
+
+                    {currentUser?._id === user._id && (
+                        <DeleteIcon size={20} cursor={'pointer'} onClick={handleDeletePost} />
+                    )}
                 </Flex>
             </Flex>
 
             <Text my={3}>{post.text}</Text>
 
-            <Box borderRadius={6} overflow={'hidden'} border={'1px solid'} borderColor={'gray.light'}>
-                <Image src={'/post1.png'} w={'full'} />
-            </Box>
+            {post.img && (
+                <Box borderRadius={6} overflow={'hidden'} border={'1px solid'} borderColor={'gray.light'}>
+                    <Image src={post.img} w={'full'} />
+                </Box>
+            )}
 
             <Flex gap={3} my={3}>
                 <Actions post={post} />
-            </Flex>
-
-            <Flex gap={2} alignItems={'center'}>
-                <Text color={'gray.light'} fontSize={'sm'}>238 replies</Text>
-                <Box w={0.5} h={0.5} borderRadius={'full'} bg={'gray.light'}></Box>
-                <Text color={'gray.light'} fontSize={'sm'}>
-                    {200} likes
-                </Text>
             </Flex>
 
             <Divider my={4} />
