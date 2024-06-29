3030259f399b59b43a48465bfb819626896d4020
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-29 09:53:03 +0300

    Update header wit logout hook

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,19 @@
-import { Flex, Image, Link, useColorMode } from '@chakra-ui/react';
-import { useRecoilValue } from 'recoil';
+import { Button, Flex, Image, Link, useColorMode } from '@chakra-ui/react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import userAtom from '../atoms/userAtom';
 import { AiFillHome } from 'react-icons/ai';
 import { RxAvatar } from 'react-icons/rx';
 import { Link as RouterLink } from 'react-router-dom';
+import { FiLogOut } from 'react-icons/fi';
+import useLogout from '../hooks/useLogout';
+import authScreenAtom from '../atoms/authAtom';
 
 const Header = () => {
     const { colorMode, toggleColorMode } = useColorMode();
     const user = useRecoilValue(userAtom);
+    const logout = useLogout();
+    const setAuthScreen = useSetRecoilState(authScreenAtom);
+
 
     return (
         <Flex justifyContent={'space-between'} mt={6} mb='12'>
@@ -16,6 +22,13 @@ const Header = () => {
                     <AiFillHome size={24} />
                 </Link>
             )}
+
+            {!user && (
+                <Link as={RouterLink} to={'/auth'}  onClick={() => setAuthScreen('login')}>
+                    Login
+                </Link>
+            )}
+
             <Image
                 cursor={'pointer'}
                 alt='logo'
@@ -29,7 +42,16 @@ const Header = () => {
                     <Link as={RouterLink} to={`/${user.username}`}>
                         <RxAvatar size={24} />
                     </Link>
+                    <Button size={'xs'} onClick={logout}>
+                        <FiLogOut size={20} />
+                    </Button>
                 </Flex>
+            )}
+
+            {!user && (
+                <Link as={RouterLink} to={'/auth'} onClick={() => setAuthScreen('signup')}>
+                    Sign up
+                </Link>
             )}
         </Flex>
     );
