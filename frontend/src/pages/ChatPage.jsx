c2bd7761bab2c56137d4af8cf4d1be6c0ee6167f
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-07-08 15:26:53 +0300

    Added handle conversation search

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,17 +1,33 @@
 import { useEffect, useState } from 'react';
 import { SearchIcon } from '@chakra-ui/icons';
-import { Box, Button, Flex, Input, Skeleton, SkeletonCircle, Text, useColorModeValue } from '@chakra-ui/react';
+import {
+    Box,
+    Button,
+    Flex,
+    Input,
+    Skeleton,
+    SkeletonCircle,
+    Text,
+    useColorModeValue
+} from '@chakra-ui/react';
 import Conversation from '../components/Conversation';
 import { GiConversation } from 'react-icons/gi';
 import MessageContainer from '../components/MessageContainer';
 import useShowToast from '../hooks/useShowToast';
-import { useRecoilState } from 'recoil';
-import { conversationsAtom, selectedConversationAtom } from '../atoms/messagesAtom';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import {
+    conversationsAtom,
+    selectedConversationAtom,
+} from '../atoms/messagesAtom';
+import userAtom from '../atoms/userAtom';
 
 const ChatPage = () => {
     const [loadingConversations, setLoadingConversations] = useState(true);
+    const [searchText, setSearchText] = useState('');
     const [conversations, setConversations] = useRecoilState(conversationsAtom);
     const [selectedConversation, setSelectedConversation] = useRecoilState(selectedConversationAtom);
+    const [searchingUser, setSearchingUser] = useState(false);
+    const currentUser = useRecoilValue(userAtom);
 
     const showToast = useShowToast();
 
@@ -36,6 +52,31 @@ const ChatPage = () => {
         getConversations();
     }, [showToast, setConversations]);
 
+    const handleConversationSearch = async (e) => {
+        e.preventDefault();
+        setSearchingUser(true);
+
+        try {
+            const res = await fetch(`/api/users/profile/${searchText}`);
+            const searchedUser = await res.json();
+
+            if (searchedUser.error) {
+                showToast('Error', searchedUser.error, 'error');
+                return;
+            }
+
+            const messagingYourself = searchedUser._id === currentUser._id;
+            if (messagingYourself) {
+                showToast('Error', 'You cannot message yourself', 'error');
+                return;
+            }
+        } catch (error) {
+            showToast('Error', error.message, 'error');
+        } finally {
+            setSearchingUser(false);
+        }
+    };
+
     return (
         <Box
             position={'absolute'}
@@ -53,14 +94,23 @@ const ChatPage = () => {
                 }}
                 mx={'auto'}
             >
-                <Flex flex={30} gap={2} flexDirection={'column'} maxW={{ sm: '250px', md: 'full' }} mx={'auto'}>
-                    <Text fontWeight={700} color={useColorModeValue('gray.600', 'gray.400')}>
+                <Flex
+                    flex={30}
+                    gap={2}
+                    flexDirection={'column'}
+                    maxW={{ sm: '250px', md: 'full' }}
+                    mx={'auto'}
+                >
+                    <Text
+                        fontWeight={700}
+                        color={useColorModeValue('gray.600', 'gray.400')}
+                    >
                         Your Conversations
                     </Text>
-                    <form >
+                    <form onSubmit={handleConversationSearch}>
                         <Flex alignItems={'center'} gap={2}>
-                            <Input placeholder='Search for a user' />
-                            <Button size={'sm'} >
+                            <Input placeholder='Search for a user' onChange={(e) => setSearchText(e.target.value)} />
+                            <Button size={'sm'} onClick={handleConversationSearch} isLoading={searchingUser}>
                                 <SearchIcon />
                             </Button>
                         </Flex>
@@ -68,7 +118,13 @@ const ChatPage = () => {
 
                     {loadingConversations &&
                         [0, 1, 2, 3, 4].map((_, i) => (
-                            <Flex key={i} gap={4} alignItems={'center'} p={'1'} borderRadius={'md'}>
+                            <Flex
+                                key={i}
+                                gap={4}
+                                alignItems={'center'}
+                                p={'1'}
+                                borderRadius={'md'}
+                            >
                                 <Box>
                                     <SkeletonCircle size={'10'} />
                                 </Box>
@@ -79,32 +135,31 @@ const ChatPage = () => {
                             </Flex>
                         ))}
 
-                    {!loadingConversations && (
+                    {!loadingConversations &&
                         conversations.map((conversation) => (
                             <Conversation
                                 key={conversation._id}
                                 conversation={conversation}
                             />
-                        ))
-                    )}
+                        ))}
                 </Flex>
 
-                {!selectedConversation._id && (             
-                <Flex
-                    flex={70}
-                    borderRadius={'md'}
-                    p={2}
-                    flexDir={'column'}
-                    alignItems={'center'}
-                    justifyContent={'center'}
-                    height={'400px'}
-                >
-                    <GiConversation size={100} />
-                    <Text fontSize={20}>Select a conversation to start messaging</Text>
-                </Flex>
+                {!selectedConversation._id && (
+                    <Flex
+                        flex={70}
+                        borderRadius={'md'}
+                        p={2}
+                        flexDir={'column'}
+                        alignItems={'center'}
+                        justifyContent={'center'}
+                        height={'400px'}
+                    >
+                        <GiConversation size={100} />
+                        <Text fontSize={20}>Select a conversation to start messaging</Text>
+                    </Flex>
                 )}
 
-{selectedConversation._id && <MessageContainer />}
+                {selectedConversation._id && <MessageContainer />}
             </Flex>
         </Box>
     );
