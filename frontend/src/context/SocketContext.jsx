a27eceb0f78f09d413df24df67bfb119869a46e4
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-07-09 15:17:02 +0300

    Connenct and disconnect socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -11,19 +11,25 @@ export const useSocket = () => {
 
 export const SocketContextProvider = ({ children }) => {
     const [socket, setSocket] = useState(null);
+    const [onlineUsers, setOnlineUsers] = useState([]);
     const user = useRecoilValue(userAtom);
 
     useEffect(() => {
         const socket = io('http://localhost:5000', {
             query: {
                 userId: user?._id,
-            },
+            }
         });
 
         setSocket(socket);
 
+        socket.on('getOnlineUsers', (users) => {
+            setOnlineUsers(users);
+        });
+
         return () => socket && socket.close();
     }, [user?._id]);
+    console.log(onlineUsers, 'Online users');
 
-    return <SocketContext.Provider value={{ socket }}>{children}</SocketContext.Provider>;
+    return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
 };
